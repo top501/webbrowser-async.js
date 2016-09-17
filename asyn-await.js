1213f4aa23f59b6b111af6c1bc6211748c1bf663
waitaction <350561378@qq.com>
2016-09-17 19:32:42 +0800

    优化了调用

--- asyn-await.js
+++ asyn-await.js
@@ -1,71 +1,117 @@
-var Asyn = (function () {
+var AsyncAwait = (function () {
 
-    function Asyn() {
+    function AsyncAwait() {
         this.currAsynResult = {};
         this.preAsynResult = {};
         this.scope = {};
+        this._taskAsyn = [];
+        this._taski = -1;
+        this._taskResultIndex = -1
     }
 
-    var _taskAsyn = [];
-    var _taski = -1;
-    var _taskResultIndex = -1
-
-    Asyn.prototype.await = function (func, extend) {
+    AsyncAwait.prototype.$go =
+    AsyncAwait.prototype.$using =
+    AsyncAwait.prototype.$code =
+    AsyncAwait.prototype.$getResult =
+    AsyncAwait.prototype.$await = function (func, extend) {
         var that = this;
         var $scope = this.scope;
-        _taski++;
-        _taskAsyn.push({
+        that._taski++;
+        that._taskAsyn.push({
             method: function (index) {
-                that.preAsynResult = index != 0 ? _taskAsyn[index - 1].asynResult : null;
-                func.call(this, $scope)
+                that.preAsynResult = index != 0 ? that._taskAsyn[index - 1].asynResult : null;
+                func.call(this, $scope, that.preAsynResult, []);
             },
             asynResult: null,
-            index: _taski,
+            index: that._taski,
             extend: extend
         });
         return this;
     }
-
-    Asyn.prototype.setAsynResult = function (data) {
-        _taskResultIndex++;
-        var taskData = _taskAsyn[_taskResultIndex];
+    AsyncAwait.prototype.$done =
+    AsyncAwait.prototype.setAsynResult = function (data) {
+        this._taskResultIndex++;
+        var taskData = this._taskAsyn[this._taskResultIndex];
         taskData.asynResult = data;
-        if (_taskAsyn.length > _taskResultIndex + 1) {
-            _taskAsyn[_taskResultIndex + 1].method(_taskResultIndex + 1);
+        if (this._taskAsyn.length > this._taskResultIndex + 1) {
+            this._taskAsyn[this._taskResultIndex + 1].method(this._taskResultIndex + 1);
         }
     }
-
-    Asyn.prototype.start = function () {
+    AsyncAwait.prototype.start = function () {
         //执行链式调用
-        _taskAsyn[0].method(0);
+        this._taskAsyn[0].method(0);
+    }
+    AsyncAwait.prototype.$returnResult = function (data) {
+        this.setAsynResult(data);
+    }
+    AsyncAwait.prototype.$run = function () {
+        this.start();
+    }
+    AsyncAwait.prototype.$dispose = function () {
+        this.currAsynResult = {};
+        this.preAsynResult = {};
+        this.scope = {};
+        this._taskAsyn = [];
+        this._taski = -1;
+        this._taskResultIndex = -1
+    }
+    AsyncAwait.prototype.$if = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$then = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$else = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$endif = function () {
+        return this;
+    }
+    AsyncAwait.prototype.$for = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$to = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$body = function (func) {
+        return this;
+    }
+    AsyncAwait.prototype.$next = function (func) {
+        return this;
     }
 
-    return Asyn;
+    return AsyncAwait;
 
 }());
-//=======进一步简化操作=======
 
+function $async(func) {
 
-var __asyn = {}
+    var asyncAwait = new AsyncAwait();
+    var scope = {};
 
-function _asyn(func) {
-    __asyn = new Asyn();
-    func.call(__asyn);
-    __asyn.start();
+    var using = function (_func, extend) {
+        asyncAwait.$await(_func, extend);
+    };
+
+    var done = function () {
+        asyncAwait.$done();
+    };
+
+    var dispose = function () {
+        asyncAwait.$dispose();
+    };
+
+    func(scope, using, done, dispose);
+
+    asyncAwait.$run();
 }
 
-function await(_func, extend) {
-    __asyn.await(_func, extend)
-}
 
-function getPreResult() {
-    return __asyn.preAsynResult;
-}
 
-function setCurrResult(data) {
-    __asyn.setAsynResult(data);
-}
 
-function nextAwait(data) {
-    __asyn.setAsynResult(data);
-}
+
+
+
+
+
+
